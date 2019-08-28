2a5469965dc46bda92d96ef65460a9f2ae5ec21a
andrew <andrew.r.geiger@gmail.com>
2019-08-27 22:02:24 -0400

    Merge brings together recordsets! This allows them to be consolidated   into monolithic computations, if desired. More though, is simply   bringing others into one canonical set, ala checkpoint.   Just map a previous group on it to regroup it.

--- shared/data/transform.py
+++ shared/data/transform.py
@@ -74,8 +74,58 @@ class Merge(Transform):
     The new record type will have all the source columns,
       with the caveat that later sources win for overlaps.
     """
-    pass
-    #raise NotImplementedError
+    ScanClass = ElementScanner
+    
+    def __init__(self, sources):
+        # Initialize mixins
+        super(Merge, self).__init__()
+        self.sources = tuple(sources)
+        self._resolveSources()
+        
+    
+    def _resolveSources(self):
+        """Sources may overlap: if so, only take the latter."""        
+        rawSources = [source.results if isinstance(source, Composable) else source
+                      for source 
+                      in self.sources]
+        
+        allFields = []
+        # Gather all the fields
+        for source in rawSources:
+            for field in source._RecordType._fields:
+                allFields.append(field)
+        
+        scanners = []
+        sourceFields = set(allFields)
+        for source in reversed(rawSources):
+            for field in source._RecordType._fields:
+                if field in sourceFields:
+                    sourceFields.remove(field)
+                    scanners.append( (field, self.ScanClass(source, field)) )
+                if not sourceFields:
+                    break
+            if not sourceFields:
+                break
+            
+        # While we want to prioritize later sources, the fields should
+        #   likely keep the same order, starting with the earlier sources.
+        # see https://stackoverflow.com/a/12814719/1943640
+        scanners.sort(key=lambda entry: allFields.index(entry[0]))
+        
+        self.scanners = tuple(scanner 
+                              for field,scanner 
+                              in scanners)
+        self._resultSet = RecordSet(recordType=genRecordType(field 
+                                                             for field,scanner 
+                                                             in scanners))
+        
+    def transform(self):
+        """Simply scan down the sources, generating new records."""
+        self._resultSet.append( tuple(
+            self._resultSet.coerceRecordType(newRecordValues)
+            for newRecordValues
+            in zip(*self.scanners)
+            ) )        
 
 
 class Unvacuum(Transform):
