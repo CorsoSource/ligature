7de6218ea7c18943aa0e4eaccd6668ec660d7907
Andrew Geiger <andrew.geiger@corsosystems.com>
2020-03-29 21:03:33 -0400

    Updating for PyPI reference

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ligature",
+    version="1.1.0",
+    author="Andrew Geiger",
+    author_email="andrew.geiger@corsosystems.com",
+    description="A toy Python library for merging data, from low to high level abstractions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CorsoSource/ligature",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.5',
+)
